53346ba0e44d5b2ada27359361326ad5832afe00
hwido <hyudojung@naver.com>
2023-07-11 16:58:20 +0900

    link to firebase. change page with button click

--- app/src/main/java/com/hwido/football/MainActivity.kt
+++ app/src/main/java/com/hwido/football/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.hwido.football
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+
+        auth = Firebase.auth
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+            val ID = findViewById<EditText>(R.id.IDArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.signInWithEmailAndPassword(ID.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        val joininBtn = findViewById<Button>(R.id.joinBtn)
+        joininBtn.setOnClickListener {
+            val intent = Intent(this, signin::class.java)
+            startActivity(intent)
+        }
     }
 }
