4db03c2bc8dde7d72d8093ac8fcbcc84805a5e07
hwido <hyudojung@naver.com>
2023-07-25 22:35:32 +0900

    combine with each one

--- app/src/main/java/com/hwido/football/LoginIndividualActivity.kt
+++ app/src/main/java/com/hwido/football/LoginIndividualActivity.kt
@@ -2,18 +2,19 @@ package com.hwido.football
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class LoginIndividualActivity : Fragment() {
+class LoginIndividualActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val database = Firebase.database
     }
 
     override fun onCreateView(
