4578f9cfbf4866e1a3b3b205cee841db051d28ba
hwido <hyudojung@naver.com>
2023-08-27 18:25:12 +0900

    page change by shangjeen

--- app/src/main/java/com/hwido/football/MainMainpage.kt
+++ app/src/main/java/com/hwido/football/MainMainpage.kt
@@ -1,9 +1,11 @@
 package com.hwido.football
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 
@@ -16,6 +18,26 @@ class MainMainpage : AppCompatActivity(), View.OnClickListener{
         val fragmentTransaction = fm.beginTransaction()
         fragmentTransaction.add(R.id.main_mainpage_fragment, MainMainpageFragmentSchedule())
         fragmentTransaction.commit()
+
+        // 하단 버튼
+        val mainBtnmain = findViewById<Button>(R.id.main_button_main)
+        val mainBtnteam = findViewById<Button>(R.id.main_button_team)
+        val mainBtnIndividual = findViewById<Button>(R.id.main_button_individual)
+        val mainBtnSetting = findViewById<Button>(R.id.main_button_setting)
+
+        mainBtnmain.setOnClickListener {
+        }
+
+        mainBtnteam.setOnClickListener {
+            val intent = Intent(baseContext, MainTeampage::class.java)
+            startActivity(intent)
+        }
+
+        mainBtnIndividual.setOnClickListener {
+        }
+
+        mainBtnSetting.setOnClickListener {
+        }
     }
 
     override fun onClick(v: View?) {
---
+++ app/src/main/java/com/hwido/football/MainSelectButton.kt
@@ -0,0 +1,13 @@
+package com.hwido.football
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
+
+
+class MainSelectButton {
+
+
+
+}
