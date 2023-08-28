4e6a7067053157ffd7d5c9754812fc2c597b6ba2
hwido <hyudojung@naver.com>
2023-08-28 18:22:59 +0900

    try to show ranking

--- app/src/main/java/com/hwido/football/MainMainpageFragmentIndividualrecord.kt
+++ app/src/main/java/com/hwido/football/MainMainpageFragmentIndividualrecord.kt
@@ -4,10 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainMainpageFragmentIndividualrecord : Fragment() {
+
+    //private lateinit var rankingsRef : DatabaseReference
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.main_mainpage_fragment_individualrecord, container, false)
     }
+
+//    override fun onCreate(savedInstanceState : Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.main_mainpage_fragment_individualrecord)
+//
+//        rankingsRef = FirebaseDatabase.getInstance().getReference("Rankings")
+//
+//        rankingsRef.orderByChild("score").addListenerForSingleValueEvent(object :
+//            ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                // Handle retrieved data
+//                for (rankingSnapshot in dataSnapshot.children) {
+//                    val playerName = rankingSnapshot.child("name").getValue(String::class.java)
+//                    val playerScore = rankingSnapshot.child("score").getValue(Long::class.java)
+//
+//                    // Now you can use playerName and playerScore to display the ranking in your UI
+//                }
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Handle database error
+//            }
+//        })
+//    }
 }
